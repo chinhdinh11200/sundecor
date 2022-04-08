aafbd6ce658314bc959c2c0defbd493ebeb108f8
Phạm Tuấn Nghĩa <phamnghia0907@gmail.com>
2022-04-08 22:48:47 +0700

    update code

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -176,93 +176,95 @@
                 </div>
             </div>
         @else
-                <div class="product__block product__block--normal">
-                    <div class="product__block--title">
-                    <h2>{{ $menu->name }}</h2>
-                    <div class="product__block--line"></div>
-                    {{-- <div class="product__block--link"><a href="">Xem tất cả</a></div> --}}
-                    </div>
-                    <div class="product__block--list row">
-                        @foreach ($products as $product)
-                            <div class="product__block--item col-6 col-sm-4 col-md-3">
-                                <a href="{{ route('category', $product['slug']) }}">
-                                <div class="card__product">
-                                    @if (isset($product['product_size'][0]) && !$product['is_contact_product'])
-                                        <div class="card__product--icon">
-                                            <p class="p">
-                                                {{floor(($product['product_size'][0]['sell_price'] - $product['product_size'][0]['sale_price']) * 100/$product['product_size'][0]['sell_price']).'%'}}
-                                            </p>
-                                        </div>
-                                    @endif
-                                    <div class="card__product--img"><img
-                                        src="{{ asset('upload/images/product/'. $product['image_1']) }}" alt="{{ $product['name'] }}" /></div>
-                                    <h3 class="card__product--name">{{ $product['name'] }}</h3>
-                                    <div class="card__product--price d-flex justify-content-between align-items-center">
-                                        @if (!($product['is_contact_product']))
-                                            @if (isset($product['product_size'][0]))
-                                                <div class="card__product--promotional">{{ number_format($product['product_size'][0]['sale_price']) }}đ</div>
-                                                <span class="card__product--cost">{{ number_format($product['product_size'][0]['sell_price']) }}đ</span>
-                                            @endif
-                                        @else
-                                            <div class="card__product--contact">Giá liên hệ : </div>
-                                            <span class="card__product--phone">{{ $webInfo->hotline }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                                </a>
-                            </div>
-                        @endforeach
-
-                        <div class="box-trang">
-                            {{$products->links('pagination::bootstrap-4')}}
-                        </div>
-                    </div>
-                </div>
-        @endif
-        <div class="product__block product__block--hot">
-            <div class="product__block--title">
-                <h2>{{ $product_hot2s->name }}</h2>
+            <div class="product__block product__block--normal">
+                <div class="product__block--title">
+                <h2>{{ $menu->name }}</h2>
                 <div class="product__block--line"></div>
-                <div class="product__block--link"><a href="{{ route('category', $product_hot2s->slug) }}">Xem tất cả</a></div>
-            </div>
-            <div class="swiper productSwiper">
-                <div class="swiper-wrapper">
-                    @foreach ($product_hot2s->products as $product_hot)
-                        <div class="swiper-slide">
-                            <div class="product__block--item col-12">
-                                <a href="{{ route('category', $product_hot->slug) }}">
-                                    <div class="card__product">
-                                        @if ($product_hot->product_size()->first())
-                                            <div class="card__product--icon">
-                                                <p class="p">
-                                                    {{floor(($product_hot->product_size()->first()->sell_price - $product_hot->product_size()->first()->sale_price) * 100/$product_hot->product_size()->first()->sell_price).'%'}}
-                                                </p>
-                                            </div>
-                                        @endif
-                                        <div class="card__product--img"><img src="{{ asset('upload/images/product/'. $product_hot->image_1) }}" alt="{{ $product_hot->name }}" /></div>
-                                        <h3 class="card__product--name">{{ $product_hot->name }}</h3>
-                                        <div class="card__product--price d-flex justify-content-between align-items-center">
-                                            @if (!($product_hot->is_contact_product))
-                                                @if (($product_hot->product_size()->first()))
-                                                    <div class="card__product--promotional">{{ number_format($product_hot->product_size()->first()->sale_price) }}đ</div>
-                                                    <span class="card__product--cost">{{ number_format($product_hot->product_size()->first()->sell_price) }}đ</span>
-                                                @endif
-                                            @else
-                                                <div class="card__product--contact">Giá liên hệ : </div>
-                                                <span class="card__product--phone">{{ $webInfo->hotline }}</span>
-                                            @endif
-                                        </div>
+                {{-- <div class="product__block--link"><a href="">Xem tất cả</a></div> --}}
+                </div>
+                <div class="product__block--list row">
+                    @foreach ($products as $product)
+                        <div class="product__block--item col-6 col-sm-4 col-md-3">
+                            <a href="{{ route('category', $product['slug']) }}">
+                            <div class="card__product">
+                                @if (isset($product['product_size'][0]) && !$product['is_contact_product'])
+                                    <div class="card__product--icon">
+                                        <p class="p">
+                                            {{floor(($product['product_size'][0]['sell_price'] - $product['product_size'][0]['sale_price']) * 100/$product['product_size'][0]['sell_price']).'%'}}
+                                        </p>
                                     </div>
-                                </a>
+                                @endif
+                                <div class="card__product--img"><img
+                                    src="{{ asset('upload/images/product/'. $product['image_1']) }}" alt="{{ $product['name'] }}" /></div>
+                                <h3 class="card__product--name">{{ $product['name'] }}</h3>
+                                <div class="card__product--price d-flex justify-content-between align-items-center">
+                                    @if (!($product['is_contact_product']))
+                                        @if (isset($product['product_size'][0]))
+                                            <div class="card__product--promotional">{{ number_format($product['product_size'][0]['sale_price']) }}đ</div>
+                                            <span class="card__product--cost">{{ number_format($product['product_size'][0]['sell_price']) }}đ</span>
+                                        @endif
+                                    @else
+                                        <div class="card__product--contact">Giá liên hệ : </div>
+                                        <span class="card__product--phone">{{ $webInfo->hotline }}</span>
+                                    @endif
+                                </div>
                             </div>
+                            </a>
                         </div>
                     @endforeach
-                </div>
 
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+                    <div class="box-trang">
+                        {{$products->links('pagination::bootstrap-4')}}
+                    </div>
+                </div>
             </div>
-        </div>
+        @endif
+        @if (isset($product_hots) && $menu->menu_type_id != 4)
+            <div class="product__block product__block--hot">
+                <div class="product__block--title">
+                    <h2>{{ $product_hot2s->name }}</h2>
+                    <div class="product__block--line"></div>
+                    <div class="product__block--link"><a href="{{ route('category', $product_hot2s->slug) }}">Xem tất cả</a></div>
+                </div>
+                <div class="swiper productSwiper">
+                    <div class="swiper-wrapper">
+                        @foreach ($product_hot2s->products as $product_hot)
+                            <div class="swiper-slide">
+                                <div class="product__block--item col-12">
+                                    <a href="{{ route('category', $product_hot->slug) }}">
+                                        <div class="card__product">
+                                            @if ($product_hot->product_size()->first())
+                                                <div class="card__product--icon">
+                                                    <p class="p">
+                                                        {{floor(($product_hot->product_size()->first()->sell_price - $product_hot->product_size()->first()->sale_price) * 100/$product_hot->product_size()->first()->sell_price).'%'}}
+                                                    </p>
+                                                </div>
+                                            @endif
+                                            <div class="card__product--img"><img src="{{ asset('upload/images/product/'. $product_hot->image_1) }}" alt="{{ $product_hot->name }}" /></div>
+                                            <h3 class="card__product--name">{{ $product_hot->name }}</h3>
+                                            <div class="card__product--price d-flex justify-content-between align-items-center">
+                                                @if (!($product_hot->is_contact_product))
+                                                    @if (($product_hot->product_size()->first()))
+                                                        <div class="card__product--promotional">{{ number_format($product_hot->product_size()->first()->sale_price) }}đ</div>
+                                                        <span class="card__product--cost">{{ number_format($product_hot->product_size()->first()->sell_price) }}đ</span>
+                                                    @endif
+                                                @else
+                                                    <div class="card__product--contact">Giá liên hệ : </div>
+                                                    <span class="card__product--phone">{{ $webInfo->hotline }}</span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                </div>
+            </div>
+        @endif
         <div class="textmore__block">
             <div class="textmore__block--content textmore__block--content2">
                 <span class="textmore__block--overlay"></span>
